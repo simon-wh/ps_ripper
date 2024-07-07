bee5b4bf45713cb9f89ca9fc3d1f144996c94119
simon-wh <simon@wooting.io>
2024-07-07 21:11:53 +0200

    Invert ps2 disc type selection

--- ps2_ripper.bash
+++ ps2_ripper.bash
@@ -22,24 +22,24 @@ do
     APP_ID=$(echo ${DISCDATA} | xargs -n1 echo | grep ^ID_FS_APPLICATION_ID= | cut -d = -f2)
     ID_FS_TYPE=$(echo ${DISCDATA} | xargs -n1 echo | grep ^ID_FS_TYPE= | cut -d = -f2)
 
-    if [ "${APP_ID}" == "PLAYSTATION" ]; then
-	echo "Type is CD, creating bin/cue"
-	./rip_bincue.bash ${DRIVE} ${RIP_PATH} ${TEMP_STOR} ${APP_ID}_2
-    elif [ "${ID_FS_TYPE}" == "udf" ]; then
-	echo "Type is UDF, assuming PS2 DVD"
-	mkdir -p ${RIP_PATH}/PLAYSTATION_2
-	ddrescue -b 2048 ${DRIVE} ${RIP_PATH}/ps2_temp_iso.iso
-	# now do something cool with python
-	DISCNAME=$(./get_ps2_name.py ${RIP_PATH}/ps2_temp_iso.iso)
-	# move the file to PLAYSTATION_2
-	if [ -f "${RIP_PATH}/PLAYSTATION_2/${DISCNAME}.iso" ]; then
-	    echo "File already exists, making copy"
-	    DS=$(date +%s)
-	    mv ${RIP_PATH}/ps2_temp_iso.iso "${RIP_PATH}/PLAYSTATION_2/${DISCNAME}_${DS}.iso"
-	else
-	    mv ${RIP_PATH}/ps2_temp_iso.iso "${RIP_PATH}/PLAYSTATION_2/${DISCNAME}.iso"
-	fi
-	echo "Finished ${DISCNAME}"
+    if [ "${ID_FS_TYPE}" == "udf" ]; then
+       	echo "Type is UDF, assuming PS2 DVD"
+       	mkdir -p ${RIP_PATH}/PLAYSTATION_2
+       	ddrescue -b 2048 ${DRIVE} ${RIP_PATH}/ps2_temp_iso.iso
+       	# now do something cool with python
+       	DISCNAME=$(./get_ps2_name.py ${RIP_PATH}/ps2_temp_iso.iso)
+       	# move the file to PLAYSTATION_2
+       	if [ -f "${RIP_PATH}/PLAYSTATION_2/${DISCNAME}.iso" ]; then
+       	    echo "File already exists, making copy"
+       	    DS=$(date +%s)
+       	    mv ${RIP_PATH}/ps2_temp_iso.iso "${RIP_PATH}/PLAYSTATION_2/${DISCNAME}_${DS}.iso"
+       	else
+       	    mv ${RIP_PATH}/ps2_temp_iso.iso "${RIP_PATH}/PLAYSTATION_2/${DISCNAME}.iso"
+       	fi
+       	echo "Finished ${DISCNAME}"
+    elif [ "${APP_ID}" == "PLAYSTATION" ]; then
+       	echo "Type is CD, creating bin/cue"
+       	./rip_bincue.bash ${DRIVE} ${RIP_PATH} ${TEMP_STOR} ${APP_ID}_2
     fi
 
     sleep 5
